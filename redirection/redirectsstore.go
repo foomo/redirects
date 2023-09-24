dadd4f17ae327c2ce22de76d804118076d37d5e8
Dragana Berber Krizik <dragana.berber.krizik@bestbytes.com>
2023-09-24 15:51:37 +0200

    Implemented functions in redirects store

--- redirection/redirectsstore.go
+++ redirection/redirectsstore.go
@@ -4,10 +4,10 @@ import (
 	"context"
 
 	keelmongo "github.com/foomo/keel/persistence/mongo"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
-	"gopkg.in/mgo.v2/bson"
 )
 
 type RedirectsStore struct {
@@ -39,21 +39,56 @@ func NewRedirectsStore(l *zap.Logger, persistor *keelmongo.Persistor) (rs *Redir
 }
 
 func (rs RedirectsStore) Find(ctx context.Context, id string) (*RedirectDefinition, error) {
-	// TODO: Implement
-	return nil, nil
+	var result RedirectDefinition
+	findErr := rs.collection.FindOne(ctx, bson.M{"id": id}, &result)
+	if findErr != nil {
+		return nil, findErr
+	}
+	return &result, nil
 }
 
 func (rs RedirectsStore) Insert(ctx context.Context, def *RedirectDefinition) error {
-	// TODO: Implement
-	return nil
+	_, err := rs.collection.Col().InsertOne(ctx, def)
+	return err
 }
 
 func (rs RedirectsStore) Update(ctx context.Context, def *RedirectDefinition) error {
-	// TODO: Implement
-	return nil
+	filter := bson.D{{Key: "id", Value: def.ID}}
+	update := bson.D{{Key: "$set", Value: def}}
+
+	_, err := rs.collection.Col().UpdateOne(ctx, filter, update)
+	return err
+
+}
+
+// maybe will be needed for migrating manual redirections?
+func (rs RedirectsStore) UpsertMany(ctx context.Context, defs []*RedirectDefinition) error {
+
+	var operations []mongo.WriteModel
+
+	for _, def := range defs {
+		operation := mongo.NewUpdateOneModel()
+		operation.SetFilter(bson.M{
+			"id": def.ID,
+		})
+		operation.SetUpdate(bson.D{{Key: "$set", Value: def}})
+		operation.SetUpsert(true)
+		operations = append(operations, operation)
+	}
+	bulkOption := options.BulkWriteOptions{}
+	bulkOption.SetOrdered(false)
+
+	_, err := rs.collection.Col().BulkWrite(ctx, operations, &bulkOption)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
 
 func (rs RedirectsStore) Delete(ctx context.Context, id string) error {
-	// TODO: Implement
-	return nil
+	filter := bson.D{{Key: "id", Value: id}}
+
+	_, err := rs.collection.Col().DeleteOne(ctx, filter)
+	return err
 }
