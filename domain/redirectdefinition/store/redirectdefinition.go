8ab144bbe2770f83277368e0aeaa48907dd45e9c
Dragana Berber Krizik <dragana.berber.krizik@bestbytes.com>
2023-10-06 12:37:36 +0200

    Added redirection type to definition

--- domain/redirectdefinition/store/redirectdefinition.go
+++ domain/redirectdefinition/store/redirectdefinition.go
@@ -4,14 +4,21 @@ type RedirectSource string
 type RedirectID string
 type RedirectTarget string
 type RedirectRequest string
+type RedirectionType string
+
+const (
+	Manual    RedirectionType = "manual"
+	Automatic RedirectionType = "automatic"
+)
 
 type RedirectDefinition struct {
-	ID             RedirectID     `json:"id" bson:"id"`
-	Source         RedirectSource `json:"source" bson:"source"`
-	Target         RedirectTarget `json:"target" bson:"target"`
-	Code           RedirectCode   `json:"code" bson:"code"`
-	RespectParams  bool           `json:"respectparams" bson:"respectparams"`
-	TransferParams bool           `json:"transferparams" bson:"transferparams"`
+	ID              RedirectID      `json:"id" bson:"id"`
+	Source          RedirectSource  `json:"source" bson:"source"`
+	Target          RedirectTarget  `json:"target" bson:"target"`
+	Code            RedirectCode    `json:"code" bson:"code"`
+	RespectParams   bool            `json:"respectparams" bson:"respectparams"`
+	TransferParams  bool            `json:"transferparams" bson:"transferparams"`
+	RedirectionType RedirectionType `json:"redirectType" bson:"redirectType"`
 }
 
 type RedirectDefinitions map[RedirectSource]*RedirectDefinition
