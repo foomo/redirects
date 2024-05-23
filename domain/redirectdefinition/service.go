caf77c78e7c52ed81a5a060f9bd186c754bf821d
Daniel Thomas <daniel.thomas@bestbytes.com>
2024-05-23 13:40:00 +0200

    feat: wire correct error types

--- domain/redirectdefinition/service.go
+++ domain/redirectdefinition/service.go
@@ -39,30 +39,46 @@ func (rs *Service) GetRedirects(w http.ResponseWriter, r *http.Request) (*redire
 	return rs.api.GetRedirects(r.Context())
 }
 
-func (rs *Service) Search(w http.ResponseWriter, r *http.Request, dimension, id, path string) (*redirectstore.RedirectDefinitions, error) {
-	return rs.api.Search(r.Context(), redirectquery.Search{
+func (rs *Service) Search(w http.ResponseWriter, r *http.Request, dimension, id, path string) (*redirectstore.RedirectDefinitions, *redirectstore.RedirectDefinitionError) {
+	result, err := rs.api.Search(r.Context(), redirectquery.Search{
 		ID:     id,
 		Source: redirectstore.RedirectSource(path),
 	})
+	if err != nil {
+		return nil, redirectstore.NewRedirectDefinitionError(err.Error())
+	}
+	return result, nil
 }
 
-func (rs *Service) Create(w http.ResponseWriter, r *http.Request, def *redirectstore.RedirectDefinition) error {
-	return rs.api.CreateRedirect(r.Context(),
+func (rs *Service) Create(w http.ResponseWriter, r *http.Request, def *redirectstore.RedirectDefinition) *redirectstore.RedirectDefinitionError {
+	err := rs.api.CreateRedirect(r.Context(),
 		redirectcommand.CreateRedirect{
 			RedirectDefinition: def,
 		})
+	if err != nil {
+		return redirectstore.NewRedirectDefinitionError(err.Error())
+	}
+	return nil
 }
 
-func (rs *Service) Delete(w http.ResponseWriter, r *http.Request, path string) error {
-	return rs.api.DeleteRedirect(r.Context(),
+func (rs *Service) Delete(w http.ResponseWriter, r *http.Request, path string) *redirectstore.RedirectDefinitionError {
+	err := rs.api.DeleteRedirect(r.Context(),
 		redirectcommand.DeleteRedirect{
 			Source: redirectstore.RedirectSource(path),
 		})
+	if err != nil {
+		return redirectstore.NewRedirectDefinitionError(err.Error())
+	}
+	return nil
 }
 
-func (rs *Service) Update(w http.ResponseWriter, r *http.Request, def *redirectstore.RedirectDefinition) error {
-	return rs.api.UpdateRedirect(r.Context(),
+func (rs *Service) Update(w http.ResponseWriter, r *http.Request, def *redirectstore.RedirectDefinition) *redirectstore.RedirectDefinitionError {
+	err := rs.api.UpdateRedirect(r.Context(),
 		redirectcommand.UpdateRedirect{
 			RedirectDefinition: def,
 		})
+	if err != nil {
+		return redirectstore.NewRedirectDefinitionError(err.Error())
+	}
+	return nil
 }
--- domain/redirectdefinition/store/error.go
+++ domain/redirectdefinition/store/error.go
@@ -2,6 +2,11 @@ package redirectstore
 
 type RedirectDefinitionError string
 
+func NewRedirectDefinitionError(err string) *RedirectDefinitionError {
+	e := RedirectDefinitionError(err)
+	return &e
+}
+
 func (r *RedirectDefinitionError) Error() string {
 	return string(*r)
 }
